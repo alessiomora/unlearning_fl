71ba1d8b4e0263a390453d10b04ce7eb2e6ff59c
Alessio Mora <mora.alessio20@gmail.com>
2023-11-13 12:27:43 +0100

    Added hf model checkpoints

--- unlearning_fl/unlearn_routine.py
+++ unlearning_fl/unlearn_routine.py
@@ -22,31 +22,22 @@ from unlearning_fl.dataset import (
 )
 from unlearning_fl.model_utility import get_transformer_model
 from unlearning_fl.fedsmoothie_model import FedSmoothieModel
+from unlearning_fl.fedmixup_model import FedMixUpModel
+from unlearning_fl.goodbad_model import GoodBadModel
+from unlearning_fl.mixup_utility import (
+    return_augmented_dataset,
+    return_bad_good_dataset,
+)
 from unlearning_fl.utils import (
     dic_load,
 )
 
+
 TEST_BATCH_SIZE = 128
 IMAGE_SIZE = 224
 
-def get_model(model_url):
-    """Load a checkpoint model from its url."""
-    inputs = tf.keras.Input((IMAGE_SIZE, IMAGE_SIZE, 3))
-    hub_module = hub.KerasLayer(model_url)
 
-    outputs, _ = hub_module(inputs)
-
-    return tf.keras.Model(inputs, outputs)
-
-
-def pil_loader(path):
-    """Load a PIL image."""
-    with open(path, 'rb') as f:
-        with Image.open(f) as img:
-            return img.convert('RGB')
-
-
-def get_optimizer(optimizer_name="adamw", lr_client=3e-4, clipnorm=None, l2_weight_decay=1e-3, momentum=0.0):
+def get_optimizer(optimizer_name="adamw", lr_client=1e-5, clipnorm=None, l2_weight_decay=1e-3, momentum=0.0):
     if optimizer_name == "adam":
         optimizer = tf.keras.optimizers.Adam(
             learning_rate=lr_client,
@@ -79,6 +70,7 @@ def get_optimizer(optimizer_name="adamw", lr_client=3e-4, clipnorm=None, l2_weig
 table_dataset_classes = {"cifar100": 100, "birds": 200, "cars": 196,
                              "aircrafts": 100}
 
+
 @hydra.main(config_path="conf", config_name="base", version_base=None)
 def main(cfg: DictConfig) -> None:  # pylint: disable=too-many-locals
     print("[Start Simulation]")
@@ -109,22 +101,56 @@ def main(cfg: DictConfig) -> None:  # pylint: disable=too-many-locals
     trainable_blocks_fe = cfg.trainable_blocks
     num_classes = table_dataset_classes[dataset]
     configs = []
-    configs[0] = [1, 0.1]
-    configs[1] = [1, 0.2]
-    configs[2] = [1, 0.3]
-    configs[3] = [1, 0.5]
-    configs[4] = [1, 0.7]
-    configs[5] = [1, 0.8]
-    configs[6] = [5, 0.1]
-    configs[7] = [5, 0.2]
-    configs[8] = [5, 0.3]
-    configs[9] = [5, 0.5]
-    configs[10] = [5, 0.7]
-    configs[11] = [5, 0.8]
+    # configs.append([1, 5e-3])  # [epochs, smoothing]
+    # configs.append([1, 1e-3])  # [epochs, smoothing]
+    # configs.append([1, 5e-4])  # [epochs, smoothing]
+    # configs.append([1, 1e-4])  # [epochs, smoothing]
+    # configs.append([1, 9e-4])  # [epochs, smoothing]
+    # configs.append([1, 7e-4])  # [epochs, smoothing]
+    # configs.append([1, 5e-4])  # [epochs, smoothing]
+    # configs.append([1, 3e-4])  # [epochs, smoothing]
+    # configs.append([1, 1e-4])  # [epochs, smoothing]
+    # configs.append([1, 9e-5])  # [epochs, smoothing]
+    # configs.append([1, 7e-5])  # [epochs, smoothing]
+    # configs.append([1, 5e-5])  # [epochs, smoothing]
+    # configs.append([1, 4e-5])  # [epochs, smoothing]
+    configs.append([1, 3e-5])  # [epochs, smoothing]
+    configs.append([1, 1e-5])  # [epochs, smoothing]
+    configs.append([1, 9e-6])  # [epochs, smoothing]
+    configs.append([1, 8e-6])  # [epochs, smoothing]
+    configs.append([1, 7e-6])  # [epochs, smoothing]
+    # configs.append([1, 9e-6])  # [epochs, smoothing]
+    # configs.append([1, 8e-6])  # [epochs, smoothing]
+    # configs.append([1, 6e-6])  # [epochs, smoothing]
 
-
+    # configs.append([1, 1e-7])
+    # configs.append([2, 1e-6])  # [epochs, smoothing]
+    # configs.append([2, 3e-6])
+    # configs.append([2, 6e-6])
+    # configs.append([2, 1e-7])
+    # configs.append([1, 0.5])
+    # configs.append([1, 0.7])
+    # configs.append([1, 0.8])
+    # configs.append([2, 0.1])
+    # configs.append([2, 0.2])
+    # configs.append([2, 0.3])
+    # configs.append([5, 0.1])
+    # configs.append([5, 0.2])
+    # configs.append([5, 0.3])
+    # configs.append([5, 0.5])
+    # configs.append([5, 0.7])
+    # configs.append([5, 0.8])
 
     client_model = get_transformer_model(
+        model_name=model_name,
+        classifier_hidden_layers=classifier_hidden_layers,
+        num_classes=num_classes,
+        random_seed=random_seed,
+        load_pretrained_weights=load_pretrained_weights,
+        trainable_feature_extractor=trainable_feature_extractor,
+        trainable_blocks_fe=trainable_blocks_fe
+    )
+    random_model = get_transformer_model(
         model_name=model_name,
         classifier_hidden_layers=classifier_hidden_layers,
         num_classes=num_classes,
@@ -177,7 +203,7 @@ def main(cfg: DictConfig) -> None:  # pylint: disable=too-many-locals
     )
 
     # path = os.path.join(save_path_checkpoints, "dict_info.pickle")
-    last_checkpoint = 50
+    last_checkpoint = 100
 
     path = os.path.join(
             save_path_checkpoints,
@@ -217,44 +243,171 @@ def main(cfg: DictConfig) -> None:  # pylint: disable=too-many-locals
             .batch(TEST_BATCH_SIZE)
     )
 
-    train_ds = load_client_datasets_from_files(
+    train_ds_for_test = load_client_datasets_from_files(
         dataset=dataset,
         sampled_client=int(cid),
         total_clients=total_clients,
         batch_size=batch_size,
         alpha=alpha_dirichlet,
         seed=random_seed,
+        is_training=False,
     )
 
-    print("[Evaluation] Model trained on Sanitized Dataset")
+    train_ds_for_train = load_client_datasets_from_files(
+        dataset=dataset,
+        sampled_client=int(cid),
+        total_clients=total_clients,
+        batch_size=batch_size,
+        alpha=alpha_dirichlet,
+        seed=random_seed,
+        is_training=True,  # enabling random augmentations
+    )
+    train_ds_other_clients = load_client_datasets_from_files(
+        dataset=dataset,
+        sampled_client=1,
+        total_clients=total_clients,
+        batch_size=batch_size,
+        alpha=alpha_dirichlet,
+        seed=random_seed,
+        is_training=True,  # enabling random augmentations
+    )
+    for i in range(2, 29):
+        train_ds_for_train_client_2 = load_client_datasets_from_files(
+            dataset=dataset,
+            sampled_client=i,
+            total_clients=total_clients,
+            batch_size=batch_size,
+            alpha=alpha_dirichlet,
+            seed=random_seed,
+            is_training=True,  # enabling random augmentations
+        )
+        train_ds_other_clients = train_ds_other_clients.concatenate(train_ds_for_train_client_2)
+
+    # train_ds_other_clients = train_ds_for_train_client_1.concatenate(train_ds_for_train_client_2)
+    train_ds_other_clients = train_ds_other_clients.unbatch().shuffle(1024).batch(32)
+
+
+    print("[Evaluation] Model trained on Sanitized Dataset (Excluding Client 0)")
     client_model.load_weights(path_sanitized)
     client_model.compile(
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
     )
-    client_model.evaluate(test_ds)
+    # print("--- Test data ---")
+    # client_model.evaluate(test_ds)
+    # print("--- Client 0 train data ---")
+    # client_model.evaluate(train_ds_for_test)
 
-    print("[Evaluation] Model before Unlearning")
+    print("[Evaluation] Model Trained Including Client 0")
     client_model.load_weights(path)
     client_model.compile(
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
     )
+    print("--- Test data ---")
     client_model.evaluate(test_ds)
+    print("--- Client 0 train data ---")
+    client_model.evaluate(train_ds_for_test)
 
     for cfg in configs:
+        print("\n")
         print("------- Unlearning -------")
         client_model.load_weights(path)
         epochs = cfg[0]
-        smoothing = cfg[1]
-        print(f"Config: epochs {epochs} smoothing {smoothing}")
-        optimizer = get_optimizer()
-        model = FedSmoothieModel(client_model)
+        lr = cfg[1]
+        print(f"[Config] epochs {epochs} lr {lr}")
+        optimizer = get_optimizer(lr_client=lr)
+        # model = FedSmoothieModel(client_model)
+        # model.compile(
+        #     optimizer=optimizer,
+        #     loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+        #     metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        #     beta=smoothing,
+        # )
+        # results = model.fit(train_ds_for_train, epochs=epochs)
+        # model.compile(
+        #     optimizer=optimizer,
+        #     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        #     metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        # )
+        # print("[Test data]")
+        # model.evaluate(test_ds)
+        # print("[Client 0 train data]")
+        # model.evaluate(train_ds_for_test)
+
+        # model = FedMixUpModel(client_model)
+        model = GoodBadModel(client_model, random_model)
+        # client_model.classifier.set_weights(random_model.classifier.get_weights())
+        # model.compile(
+        #     optimizer=optimizer,
+        #     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        #     metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        # )
+        # print("[Test data]")
+        # model.evaluate(test_ds)
+        # print("[Client 0 train data]")
+        # model.evaluate(train_ds_for_test)
+        model.compile(
+            optimizer=optimizer,
+            # loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+            loss=tf.keras.losses.KLDivergence(),
+            metrics=[tf.keras.metrics.CategoricalAccuracy(name="accuracy")],
+        )
+
+        # train_ds_mix_up = return_augmented_dataset(train_ds_for_train, num_classes=num_classes)
+        # train_ds_mix_up = train_ds_mix_up.unbatch().shuffle(1024).batch(32)
+
+        train_ds_mix_up = return_bad_good_dataset(bad_knowledge=train_ds_for_train, good_knowledge=train_ds_other_clients)
+        train_ds_mix_up = train_ds_mix_up.unbatch().shuffle(1024*24).batch(32)
+        model.fit(train_ds_mix_up, epochs=epochs)
+
         model.compile(
             optimizer=optimizer,
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
-            beta=smoothing,
         )
-        results = model.fit(train_ds, epochs=epochs, verbose=0)
+        print("[Test data]")
         model.evaluate(test_ds)
+        print("[Client 0 train data]")
+        model.evaluate(train_ds_for_test)
+
+        # client_model.compile(
+        #     optimizer=optimizer,
+        #     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        #     metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        # )
+        # client_model.fit(train_ds_other_clients, epochs=3)
+        # print("[Test data]")
+        # client_model.evaluate(test_ds)
+        # print("[Client 0 train data]")
+        # client_model.evaluate(train_ds_for_test)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+# [Round 8] Client 0 has participated
+# [Round 13] Client 0 has participated
+# [Round 14] Client 0 has participated
+# [Round 16] Client 0 has participated
+# [Round 19] Client 0 has participated
+# [Round 21] Client 0 has participated
+# [Round 22] Client 0 has participated
+# [Round 27] Client 0 has participated
+# [Round 37] Client 0 has participated
+# [Round 38] Client 0 has participated
+# [Round 39] Client 0 has participated
+# [Round 42] Client 0 has participated
+# [Round 45] Client 0 has participated
+# [Round 47] Client 0 has participated
+# [Round 59] Client 0 has participated
+# [Round 61] Client 0 has participated
+# [Round 63] Client 0 has participated
+# [Round 64] Client 0 has participated
+# [Round 65] Client 0 has participated
+# [Round 68] Client 0 has participated
+# [Round 77] Client 0 has participated
+# [Round 86] Client 0 has participated
+# [Round 92] Client 0 has participated
